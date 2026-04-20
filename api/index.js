c91ad55f32b53bacbebf07801eb5684c3b9f2ede
Beutife <104403355+Beutife@users.noreply.github.com>
2026-04-20 23:22:59 +0100

    worked on twilio response

--- api/index.js
+++ api/index.js
@@ -17,10 +17,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Firebase setup
-const serviceAccount = require('../serviceAccountKey.json');
-admin.initializeApp({
-   credential: admin.credential.cert(serviceAccount)
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // This .replace fix is vital for Vercel to read the key correctly
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  });
+}
+
 const db = admin.firestore();
 
 // Twilio setup
